0bdd3fa8ca46aa7aae5c25d06867186deca64a29
Amol Bais <amolsanjay.bais.ext@bpost.be>
2021-06-19 16:43:45 +0530

    Rotate Image

---
+++ Arrays-I/RotateImage.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        // transpose
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        
+        // now swap till middle
+        for(int i = 0; i < n;i++){
+            for(int j = 0 ; j < n/2 ; j++){
+                int temp = matrix[i][j];
+                 matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1]= temp;
+            }
+        }
+    }
+}
